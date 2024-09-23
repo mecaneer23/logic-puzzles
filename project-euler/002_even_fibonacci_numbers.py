a06945119175411c534837b797924486f865f588
mecaneer23 <mecaneer23@gmail.com>
2024-09-23 12:43:23 -0500

    feat: implement 002

---
+++ project-euler/002_even_fibonacci_numbers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# https://projecteuler.net/problem=2
+
+def even_fib_sum() -> int:
+    """
+    Returns sum of even fibonacci numbers less than 4,000,000
+
+    Uses {1, 2} as the initial values
+    """
+    max_value = 4_000_000
+    value_a = 1
+    value_b = 2
+    even_sum = value_b
+
+    while True:
+        value_a += value_b
+        if value_a > max_value:
+            break
+        if value_a % 2 == 0:
+            even_sum += value_a
+
+        value_b += value_a
+        if value_b > max_value:
+            break
+        if value_b % 2 == 0:
+            even_sum += value_b
+
+    return even_sum
+
+print(even_fib_sum())
