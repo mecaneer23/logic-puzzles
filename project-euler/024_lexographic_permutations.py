bf55012341038d30e166cd9b7f9882af8a7f0de3
mecaneer23 <mecaneer23@gmail.com>
2024-10-04 14:03:41 -0500

    feat: implement 024

---
+++ project-euler/024_lexographic_permutations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# https://projecteuler.net/problem=24
+
+from itertools import permutations
+
+def get_lexographic_permutations() -> int:
+    """
+    Return the millionth lexographic permutation
+    of the digits 0-9 inclusive
+    """
+    for index, permutation in enumerate(permutations(range(10))):
+        if index == 1_000_000:
+            final_num = 0
+            for idx, digit in enumerate(reversed(permutation)):
+                final_num += digit * 10**idx
+            return final_num
+    return 0
+
+print(get_lexographic_permutations())
