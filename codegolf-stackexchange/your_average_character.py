80bee88334db42daaf7380f5a6128e6fb8b42011
mecaneer23 <mecaneer23@gmail.com>
2024-10-12 02:40:02 -0500

    feat: implement your average character codegolf (but not a particularly short answer)

---
+++ codegolf-stackexchange/your_average_character.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# https://codegolf.stackexchange.com/questions/276081/your-average-character
+
+
+def average_char(string: str) -> str:
+    """
+    Return the average character of a string by
+    converting characters to numerical representations,
+    finding an average (to the nearest integer), and
+    converting the average back to a character.
+    """
+    lowercase_offset = 96
+    char_sum = 0
+    length = 0
+    for char in string:
+        if char.isalpha():
+            length += 1
+            char_sum += ord(char.lower()) - lowercase_offset
+    return chr(round(char_sum / length) + lowercase_offset)
+
+
+assert average_char("a") == "a", average_char("a")
+assert average_char("aaaaa") == "a"
+assert average_char("abcde") == "c"
+assert average_char("bcde") == "d"
+assert average_char("a1!b2@c") == "b", average_char("a1!b2@c")
+assert average_char("ABCde") == "c"
+assert average_char("POP") == "p"
+assert average_char("A B C") == "b"
+
+try:
+    average_char("@@@@")
+    print("Test case failed: @@@@")
+except ZeroDivisionError:
+    pass
+
+try:
+    average_char("")
+    print('Test case failed: ""')
+except ZeroDivisionError:
+    pass
