5604cdf5036b977ca5725d7a3138c3f7dad169ba
mecaneer23 <mecaneer23@gmail.com>
2024-10-13 09:22:01 -0500

    feat: implement 034 - unclear how to detect completion of problem

---
+++ project-euler/034_digit_factorials.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# https://projecteuler.net/problem=34
+
+from functools import cache
+
+@cache
+def factorial(num: int) -> int:
+    """Return the factorial of `num`"""
+    if num <= 1:
+        return num
+    return num * factorial(num - 1)
+
+
+def get_digit_factorials() -> int:
+    """
+    Return the sum of all numbers equal to the sum of the factorial of their digits
+    """
+    total = 0
+    counter = 3
+    while True:
+        digit_factorial_sum = sum(map(factorial, map(int, str(counter))))
+        if digit_factorial_sum == counter:
+            total += counter
+        counter += 1
+        if counter > 1_000_000:  # TODO: arbitrary stopping point; how can we detect the true end?
+            return total
+
+
+print(get_digit_factorials())
